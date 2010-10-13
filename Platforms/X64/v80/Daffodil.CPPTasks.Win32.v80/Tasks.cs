061f26e4a7af44e6f0d60b2e919225476b60db4f
SND\owenwengerd_cp <SND\owenwengerd_cp@37308e9d-ab76-48fb-8b0d-99fc395a6d69>
2010-10-13 09:11:25 +0000

    Beta 3: Fixed general properties bug and addressed a few other remaining issues.

---
+++ Platforms/X64/v80/Daffodil.CPPTasks.Win32.v80/Tasks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.CPPTasks;
+
+namespace Daffodil.CPPTasks.x64.v80
+{
+  public class BSCMakeA : Microsoft.Build.CPPTasks.BSCMake
+  {
+    protected override void PostProcessSwitchList()
+    {
+      base.PostProcessSwitchList();
+    }
+    protected override System.Text.Encoding ResponseFileEncoding
+    {
+      get
+      {
+        return System.Text.Encoding.ASCII;
+      }
+    }
+  }
+}
