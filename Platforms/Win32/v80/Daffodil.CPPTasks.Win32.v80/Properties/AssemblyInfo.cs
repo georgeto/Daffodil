4c56efaddaeb4c010c33f7effa803c5783d14fc7
SND\owenwengerd_cp <SND\owenwengerd_cp@37308e9d-ab76-48fb-8b0d-99fc395a6d69>
2013-10-30 13:47:08 +0000

    Updated DLL versions to ensure correct upgrade installation; corrected internal project subfolder name.

--- Platforms/Win32/v80/Daffodil.CPPTasks.Win32.v80/Properties/AssemblyInfo.cs
+++ Platforms/Win32/v80/Daffodil.CPPTasks.Win32.v80/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ManuSoft")]
 [assembly: AssemblyProduct("Daffodil.CPPTasks.Win32.v80")]
-[assembly: AssemblyCopyright("Copyright © 2011 ManuSoft")]
+[assembly: AssemblyCopyright("Copyright © 2013 ManuSoft")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.1")]
-[assembly: AssemblyFileVersion("4.0.0.1")]
+[assembly: AssemblyVersion("4.0.0.2")]
+[assembly: AssemblyFileVersion("4.0.0.2")]
